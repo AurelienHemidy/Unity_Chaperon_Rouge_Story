d62ba65bda6d39d3c62e406401bb3de749db6af7
Aurelien.hmdy <aurelien@hemidy.fr>
2020-11-05 12:03:56 +0100

    Adding movement buttons with Canvas (with script without reference to the plane)
    Adding movement buttons with Canvas (with script without reference to the plane)

---
+++ Assets/Scripts/CPR-32/MoveTerrain.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTerrain : MonoBehaviour
+{
+   public GameObject Terrain;
+   private bool moveLeft;
+   private bool moveRight;
+    void Start()
+    {
+        //Terrain = GameObject.Find("Plane");
+        moveLeft = false;
+        moveRight = false;
+    }
+    public void OnPointerDownLeft()
+    {
+        moveLeft = true;
+    }
+    public void OnPointerUpLeft()
+    {
+        moveLeft = false;
+    }
+    public void OnPointerDownRight()
+    {
+        moveRight = true;
+    }
+    public void OnPointerUpRight()
+    {
+        moveRight = false;
+    }
+
+    private void Update() {
+        if(moveLeft) {
+            //Terrain.transform.Translate(-Vector3.right * 1 * Time.deltaTime);
+            Debug.Log("Mouvement vers la gauche");
+        }
+        if(moveRight) {
+            //Terrain.transform.Translate(Vector3.right * 1 * Time.deltaTime);
+             Debug.Log("Mouvement vers la droite");
+        }
+    }
+}
