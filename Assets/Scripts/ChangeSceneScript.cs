8945fa1a95c806b1d775f82e2851d3efd60c964c
Aurelien.hmdy <aurelien@hemidy.fr>
2020-11-27 13:00:25 +0100

    add menu and loading
    add menu and loading

---
+++ Assets/Scripts/ChangeSceneScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneScript : MonoBehaviour
+{
+    [SerializeField] int LevelToLoad;
+
+    [SerializeField] bool autoIndex = true;
+
+    private void Start() {
+        if(autoIndex) {
+        LevelToLoad = SceneManager.GetActiveScene().buildIndex+1;
+        }
+    }
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.collider.tag == "Player") {
+            SceneManager.LoadScene(LevelToLoad);
+        }
+    }
+}
---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour
+{
+   public void QuitExperience() {
+    #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+    #else
+            Application.Quit();
+    #endif
+  }
+
+  public void PlayQuitExperience() {
+      SceneManager.LoadScene("Scene 1 Introduction");
+  }
+}
