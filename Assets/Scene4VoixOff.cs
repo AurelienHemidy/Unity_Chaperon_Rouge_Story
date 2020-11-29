310ef99f5661cd1055d0c344a790832e8e16458a
LUO Yaoyi <luoyaoyi@outlook.com>
2020-11-29 19:04:44 +0100

    Scenes v3 inititial
    Scenes v3 inititial

---
+++ Assets/Scene4VoixOff.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene4VoixOff : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
