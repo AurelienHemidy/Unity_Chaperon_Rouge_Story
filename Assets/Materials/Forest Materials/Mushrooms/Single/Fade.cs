6a6512ca76051834aaf41a4ec0cf762698aa1293
LUO Yaoyi <luoyaoyi@outlook.com>
2020-12-01 10:04:39 +0100

    Add Fade
    Add Fade

---
+++ Assets/Materials/Forest Materials/Mushrooms/Single/Fade.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+public class Fade : MonoBehaviour
+{
+
+    private Renderer _renderer;
+
+    private bool _isVisible = false;
+
+    void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+        _renderer.material = Instantiate<Material>(_renderer.material);
+    }
+
+    public void FadeIn(){
+        _isVisible = false;
+        StartCoroutine(AsyncFade());
+    }
+    public void FadeOut(){
+        _isVisible = true;
+        StartCoroutine(AsyncFade());
+    }
+
+    private IEnumerator AsyncFade(){
+
+        float duration = 1.0f;
+        float progress = 0.0f;
+        float time = 0.0f;
+        Color color;
+        while(time <= duration){
+            time += Time.deltaTime;
+
+            progress = time / duration;
+
+            color = _renderer.material.color;
+            color.a = _isVisible ? progress : 1-progress;
+            _renderer.material.color = color;
+
+            yield return null;
+        }
+
+        color = _renderer.material.color;
+        color.a = _isVisible ? 1 : 0;
+        _renderer.material.color = color;
+    }
+}
