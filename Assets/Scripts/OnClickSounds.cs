b4d15dc2b6c8444550f7f8b4700a53f40f33760c
Aurelien.hmdy <aurelien@hemidy.fr>
2020-12-01 20:26:50 +0100

    add new build settings and scene 7
    add new build settings and scene 7

--- Assets/Scripts/OnClickSounds.cs
+++ Assets/Scripts/OnClickSounds.cs
@@ -7,14 +7,17 @@ public class OnClickSounds : MonoBehaviour
     public LayerMask OreillesLoup;
     public LayerMask YeuxLoup;
     public LayerMask MainsLoup;
+    public LayerMask BoucheLoup;
 
     public GameObject OreillesLoupObject;
     public GameObject YeuxLoupObject;
     public GameObject MainsLoupObject;
+    public GameObject BoucheLoupObject;
 
     public AudioSource OreillesLoupSound;
     public AudioSource YeuxLoupSound;
     public AudioSource MainsLoupSound;
+    public AudioSource BoucheLoupSound;
 
     private Camera _cam;
 
@@ -35,6 +38,7 @@ public class OnClickSounds : MonoBehaviour
                 OreillesLoupSound.Play();
                 YeuxLoupSound.Stop();
                 MainsLoupSound.Stop();
+                BoucheLoupSound.Stop();
                 //Fire le son des oreilles
             }
             if(Physics.Raycast(ray, out hit, Mathf.Infinity, YeuxLoup)){
@@ -42,6 +46,7 @@ public class OnClickSounds : MonoBehaviour
                 OreillesLoupSound.Stop();
                 YeuxLoupSound.Play();
                 MainsLoupSound.Stop();
+                BoucheLoupSound.Stop();
                 //Fire le son des yeux
             }
             if(Physics.Raycast(ray, out hit, Mathf.Infinity, MainsLoup)){
@@ -49,6 +54,15 @@ public class OnClickSounds : MonoBehaviour
                 OreillesLoupSound.Stop();
                 YeuxLoupSound.Stop();
                 MainsLoupSound.Play();
+                BoucheLoupSound.Stop();
+                //Fire le son des mains
+            }
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity, BoucheLoup)){
+                Debug.Log("mains !");
+                OreillesLoupSound.Stop();
+                YeuxLoupSound.Stop();
+                MainsLoupSound.Stop();
+                BoucheLoupSound.Play();
                 //Fire le son des mains
             }
         }
